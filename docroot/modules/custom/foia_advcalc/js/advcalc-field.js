733c5af6cbaf59d4f26a03179422dfef062cad30
Aaron Manire <amanire@isovera.com>
2019-12-06 18:01:26 -0500

    FOIA-242: Limit advanced calc comment line length to < 80 characters.

--- docroot/modules/custom/foia_advcalc/js/advcalc-field.js
+++ docroot/modules/custom/foia_advcalc/js/advcalc-field.js
@@ -34,7 +34,7 @@
       }
 
       /**
-       * Check if a field has been marked as having been calculated on page load.
+       * Check if field has been marked as having been calculated on page load.
        *
        * @param {string} selector
        * @returns {boolean}
@@ -50,7 +50,7 @@
       }
 
       /**
-       * Add a class to a field to mark that it has been calculated on page load.
+       * Add class to field to mark that it has been calculated on page load.
        *
        * @param selector
        */
@@ -59,12 +59,14 @@
       }
 
       /**
-       * Compares the values of a set of fields, returning either the upper or lower bound of the set.
+       * Compares the values of a set of fields, returning either the upper or
+       * lower bound of the set.
        *
        * @param fields
        *   A jquery object containing a set of fields.
        * @param operator
-       *   Either less than or greater than depending on which boundary of the set should be retrieved.
+       *   Either less than or greater than depending on which boundary of the
+       *   set should be retrieved.
        * @returns {*}
        */
       function calculateBoundaryOfSet(fields, operator) {
@@ -133,7 +135,7 @@
        * Calculate V.A., VI.A., XII.B. Number of [type] Pending as of End of
        * Fiscal Year per agency/component.
        *
-       * e.g. req_pend_end_yr = req_pend_start_yr + req_received_yr - req_processed_yr
+       * pend_end_yr = pend_start_yr + received_yr - processed_yr
        *
        * @param {jquery} element
        *  jQuery object of field to trigger calculations for component.
@@ -159,7 +161,7 @@
        * Calculate V.A., VI.A., XII.B. Overall Number of [type] Pending as of
        * End of Fiscal Year.
        *
-       * e.g. req_pend_end_yr = req_pend_start_yr + req_received_yr - req_processed_yr
+       * pend_end_yr = pend_start_yr + received_yr - processed_yr
        *
        * @param {string} start
        *  Partial name attribute for selecting start year field.
@@ -231,13 +233,15 @@
         if (overallXTotalFees > 0 ) {
           var overallIXProcCosts = Number($("#edit-field-overall-ix-proc-costs-0-value").val());
           var overallXPercCosts = overallXTotalFees /overallIXProcCosts;
-          overallXPercCosts = Math.round(overallXPercCosts * 10000) / 10000; // Decimal format rounded to 4 places
+          // Convert to decimal format rounded to 4 places
+          overallXPercCosts = Math.round(overallXPercCosts * 10000) / 10000;
           $('#edit-field-overall-x-perc-costs-0-value').val(overallXPercCosts);
         }
       }
 
       /**
-       *  V.A. Number of Requests Pending as of End of Fiscal Year per agency/component
+       * V.A. Number of Requests Pending as of End of Fiscal Year
+       * per agency/component
        */
 
       // V.A. Number of Requests Pending as of End of Fiscal Year
@@ -261,10 +265,11 @@
       });
 
       /**
-       *  V.A. Agency Overall Number of Requests Pending as of End of Fiscal Year
+       * V.A. Agency Overall Number of Requests Pending as of End of Fiscal Year
        */
 
-      // Initialize on load: V.A. Agency Overall Number of Requests Pending as of End of Fiscal Year
+      // Initialize on load:
+      // V.A. Agency Overall Number of Requests Pending as of End of Fiscal Year
       if (!fieldIsInitialized('#edit-field-overall-req-pend-end-yr-0-value')) {
         calculateOverallPendEndYr(
           '#edit-field-overall-req-pend-start-yr-0-value',
@@ -275,7 +280,8 @@
         markFieldInitialized('#edit-field-overall-req-pend-end-yr-0-value');
       }
 
-      // Calculate on change: V.A. Agency Overall Number of Requests Pending as of End of Fiscal Year
+      // Calculate on change:
+      // V.A. Agency Overall Number of Requests Pending as of End of Fiscal Year
       $("#edit-field-overall-req-pend-start-yr-0-value, #edit-field-overall-req-received-yr-0-value, #edit-field-overall-req-processed-yr-0-value")
         .once('advCalcVAOverallReqPendEndYr')
         .change(function() {
@@ -288,10 +294,12 @@
         });
 
       /**
-       *  VI.A. Number of Requests Pending as of End of Fiscal Year per agency/component
+       * VI.A. Number of Requests Pending as of End of Fiscal Year
+       * per agency/component
        */
 
-      // Initialize VI.A. Number of Appeals Pending as of End of Fiscal Year
+      // Initialize on load:
+      // VI.A. Number of Appeals Pending as of End of Fiscal Year
       $("input[name*='field_admin_app_via']")
         .once('initAdvCalcVIAppPendEndYr')
         .filter("input[name*='field_app_pend_end_yr']")
@@ -302,6 +310,7 @@
           }
       });
 
+      // Calculate on change:
       // VI.A. Number of Appeals Pending as of End of Fiscal Year
       $("input[name*='field_admin_app_via']")
         .filter("input[name*='field_app_pend_start_yr'], input[name*='field_app_received_yr'], input[name*='field_app_processed_yr']")
@@ -313,10 +322,11 @@
       });
 
       /**
-       *  VI.A. Agency Overall Number of Requests Pending as of End of Fiscal Year
+       * VI.A. Agency Overall Number of Requests Pending as of End of Fiscal Yr
        */
 
-      // Initialize on load: VI.A. Agency Overall Number of Requests Pending as of End of Fiscal Year
+      // Initialize on load:
+      // VI.A. Agency Overall Number of Requests Pending as of End of Fiscal Yr
       if (!fieldIsInitialized('#edit-field-overall-via-app-pend-endyr-0-value')) {
         calculateOverallPendEndYr(
           '#edit-field-overall-via-app-pend-start-0-value',
@@ -327,7 +337,8 @@
         markFieldInitialized('#edit-field-overall-via-app-pend-endyr-0-value');
       }
 
-      // Calculate on change: VI.A. Agency Overall Number of Requests Pending as of End of Fiscal Year
+      // Calculate on change:
+      // VI.A. Agency Overall Number of Requests Pending as of End of Fiscal Yr
       $("#edit-field-overall-req-pend-start-yr-0-value, #edit-field-overall-req-received-yr-0-value, #edit-field-overall-req-processed-yr-0-value")
         .once('advCalcVIOverallAppPendEndYr')
         .change(function() {
@@ -339,39 +350,39 @@
             );
         });
 
-      // Fields from section VI.C.(4) to calculate Lowest Number of Days.
+      // VI.C.(4) Lowest Number of Days.
       calculateOverall('edit-field-admin-app-vic4', 'field_low_num_days', 'edit-field-overall-vic4-low-num-days-0-value', '<');
-      // Fields from section VI.C.(4) to calculate Highest Number of Days.
+      // VI.C.(4) Highest Number of Days.
       calculateOverall('edit-field-admin-app-vic4', 'field_high_num_days', 'edit-field-overall-vic4-high-num-days-0-value', '>');
 
-      // Fields from section VII.A. to calculate Lowest Number of Days.
+      // VII.A. Lowest Number of Days.
       calculateOverall('edit-field-proc-req-viia', 'field_sim_low', 'edit-field-overall-viia-sim-low-0-value', '<');
-      // Fields from section VII.A. to calculate Highest Number of Days.
+      // VII.A. Highest Number of Days.
       calculateOverall('edit-field-proc-req-viia', 'field_sim_high', 'edit-field-overall-viia-sim-high-0-value', '>');
 
-      // Fields from section VII.A. to calculate Lowest Number of Days (complex).
+      // VII.A. Lowest Number of Days (complex).
       calculateOverall('edit-field-proc-req-viia', 'field_comp_low', 'edit-field-overall-viia-comp-low-0-value', '<');
-      // Fields from section VII.A. to calculate Highest Number of Days (complex).
+      // VII.A. Highest Number of Days (complex).
       calculateOverall('edit-field-proc-req-viia', 'field_comp_high', 'edit-field-overall-viia-comp-high-0-value', '>');
 
-      // Fields from section VII.A. to calculate Lowest Number of Days (expedited).
+      // VII.A. Lowest Number of Days (expedited).
       calculateOverall('edit-field-proc-req-viia', 'field_exp_low', 'edit-field-overall-viia-exp-low-0-value', '<');
-      // Fields from section VII.A. to calculate Highest Number of Days (expedited).
+      // VII.A. Highest Number of Days (expedited).
       calculateOverall('edit-field-proc-req-viia', 'field_exp_high', 'edit-field-overall-viia-exp-high-0-value', '>');
 
-      // Fields from section VII.B. to calculate Lowest Number of Days.
+      // VII.B. Lowest Number of Days.
       calculateOverall('edit-field-proc-req-viib', 'field_sim_low', 'edit-field-overall-viib-sim-low-0-value', '<');
-      // Fields from section VII.B. to calculate Highest Number of Days.
+      // VII.B. Highest Number of Days.
       calculateOverall('edit-field-proc-req-viib', 'field_sim_high', 'edit-field-overall-viib-sim-high-0-value', '>');
 
-      // Fields from section VII.B. to calculate Lowest Number of Days (complex).
+      // VII.B. Lowest Number of Days (complex).
       calculateOverall('edit-field-proc-req-viib', 'field_comp_low', 'edit-field-overall-viib-comp-low-0-value', '<');
-      // Fields from section VII.B. to calculate Highest Number of Days (complex).
+      // VII.B. Highest Number of Days (complex).
       calculateOverall('edit-field-proc-req-viib', 'field_comp_high', 'edit-field-overall-viib-comp-high-0-value', '>');
 
-      // Fields from section VII.B. to calculate Lowest Number of Days (expedited).
+      // VII.B. Lowest Number of Days (expedited).
       calculateOverall('edit-field-proc-req-viib', 'field_exp_low', 'edit-field-overall-viib-exp-low-0-value', '<');
-      // Fields from section VII.B. to calculate Highest Number of Days (expedited).
+      // VII.B. Highest Number of Days (expedited).
       calculateOverall('edit-field-proc-req-viib', 'field_exp_high', 'edit-field-overall-viib-exp-high-0-value', '>');
 
       /**
@@ -410,12 +421,14 @@
           });
       });
 
-      // Initialize on load: X. Agency Overall Percentage of Total Costs
+      // Initialize on load:
+      // X. Agency Overall Percentage of Total Costs
       if (!fieldIsInitialized('#edit-field-overall-x-perc-costs-0-value')) {
         calculateOverallPercentCosts();
         markFieldInitialized('#edit-field-overall-x-perc-costs-0-value');
       }
 
+      // Calculate on change:
       // X. Agency Overall Percentage of Total Costs
       $("#edit-field-overall-ix-proc-costs-0-value, #edit-field-overall-x-total-fees-0-value")
         .once('advCalcOverallXPercCosts')
@@ -428,8 +441,9 @@
        * Pending at the Agency as of End of the Fiscal Year
        */
 
-      // Initialize XII.B. Number of Consultations Received from Other Agencies
-      // that were Pending at the Agency as of End of the Fiscal Year
+      // Initialize on load:
+      // XII.B. Number of Consultations Received from Other Agencies that were
+      // Pending at the Agency as of End of the Fiscal Year
       $("input[name*='field_foia_xiib']")
         .once('initAdvCalcXIIBConPendEndYr')
         .filter("input[name*='field_pend_end_yr']")
@@ -440,7 +454,9 @@
           }
       });
 
-      // XII.B. Number of Consultations Received from Other Agencies that were Pending at the Agency as of End of the Fiscal Year
+      // Calculate on change:
+      // XII.B. Number of Consultations Received from Other Agencies that were
+      // Pending at the Agency as of End of the Fiscal Year
       $("input[name*='field_foia_xiib']")
         .filter("input[name*='field_pend_start_yr'], input[name*='field_con_during_yr'], input[name*='field_proc_start_yr']")
         .each(function() {
@@ -451,10 +467,13 @@
       });
 
       /**
-       *  XII.B. Agency Overall Number of Consultations Received from Other Agencies that were Pending at the Agency as of End of the Fiscal Year
+       * XII.B. Agency Overall Number of Consultations Received from Other
+       * Agencies that were Pending at the Agency as of End of the Fiscal Year
        */
 
-      // Initialize on load: XII.B. Agency Overall Number of Consultations Received from Other Agencies that were Pending at the Agency as of End of the Fiscal Year
+      // Initialize on load: XII.B. Agency Overall Number of Consultations
+      // Received from Other Agencies that were Pending at the Agency as of End
+      // of the Fiscal Year
       if (!fieldIsInitialized('#edit-field-overall-xiib-pend-end-yr-0-value')) {
         calculateOverallPendEndYr(
           '#edit-field-overall-xiib-pend-start-yr-0-value',
@@ -465,7 +484,9 @@
         markFieldInitialized('#edit-field-overall-xiib-pend-end-yr-0-value');
       }
 
-      // Calculate on change: XII.B. Agency Overall Number of Consultations Received from Other Agencies that were Pending at the Agency as of End of the Fiscal Year
+      // Calculate on change: XII.B. Agency Overall Number of Consultations
+      // Received from Other Agencies that were Pending at the Agency as of End
+      // of the Fiscal Year
       $("#edit-field-overall-xiib-pend-start-yr-0-value, #edit-field-overall-xiib-con-during-yr-0-value, #edit-field-overall-xiib-proc-start-yr-0-value")
         .once('advCalcXIIBOverallAppPendEndYr')
         .change(function() {
